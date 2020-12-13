dabaddf4b1c7ad1cba632575022e598e977c9605
Eron9527 <1187244022@qq.com>
2020-12-13 07:17:04 +0000

    提交修改

--- src/main/java/cn/com/taiji/service/Impl/TopologyStateServiceImpl.java
+++ src/main/java/cn/com/taiji/service/Impl/TopologyStateServiceImpl.java
@@ -1,12 +1,26 @@
 package cn.com.taiji.service.Impl;
 
+import cn.com.taiji.domain.state.TopologySpeed;
+import cn.com.taiji.domain.state.TopologyState;
 import cn.com.taiji.service.TopologyStateService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TopologyStateServiceImpl implements TopologyStateService {
     @Override
     public void add() {
 
+    }
+
+    @Override
+    public List<TopologyState> getNewsState() {
+        return null;
+    }
+
+    @Override
+    public List<TopologySpeed> getNewsSpeed() {
+        return null;
     }
 }
